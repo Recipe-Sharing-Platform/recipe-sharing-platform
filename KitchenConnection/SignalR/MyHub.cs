63adff1eb1803c905947b61948de09a98531bc11
Valmira Shala <valmirashalaa@gmail.com>
2023-01-22 19:45:41 +0100

    added simple SignalR functionality

---
+++ KitchenConnection/SignalR/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR; 
+
+    public class MyHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
